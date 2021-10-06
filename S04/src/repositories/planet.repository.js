cab0a0516154fbdea3e36987c38ba44045a6f09f
Marc-Olivier Parisien <1337721@cstj.qc.ca>
2021-10-06 11:13:14 -0400

    Fin du cours 1 octobre 2021

--- S04/src/repositories/planet.repository.js
+++ S04/src/repositories/planet.repository.js
@@ -1,7 +1,9 @@
 import Planet from "../models/planet.model.js";
+import objectToDotNotation from "../libs/objecToDotNotation.sj";
 
 const ZERO_KELVIN = -273.15;
 class PlanetRepository {
+
 
     retrieveById(idPlanet) {
         return Planet.findById(idPlanet);
@@ -21,25 +23,49 @@ class PlanetRepository {
             ]
         }
         //Where discoveredBy = Skadex AND temperature > 240 AND position.y <= 500
-        return Planet.find(filter); 
+        return Planet.find(filter);
     }
 
     create(planet) {
         return Planet.create(planet);
-        
+
     }
 
     transform(planet, transformOptions = {}) {
-        if(transformOptions){
-            if(transformOptions.unit === 'c') {
+        if (transformOptions) {
+            if (transformOptions.unit === 'c') {
                 planet.temperature += ZERO_KELVIN;
                 planet.temperature = parseFloat(planet.temperature.toFixed(2));
             }
         }
-        delete planet.__v;
+
+        planet.discoveryDate = dayjs(planet.discoveryDate).format('YYYY-MM-DD');
+
+        planet.lightspeed = `${planet.position.x.toString(16)}@${planet.position.y.toString(16)}#${planet.position.x.toString(16)}`;
+
         
+
+        observation.hex = {
+            alpha:0,
+            beta:0,
+            gamma:0,
+            delta:0,
+        }
+
+        delete planet.__v;
+
         return planet;
     }
+
+    update(idPlanet, planetModifs) {
+        const planetToDotNotation = objectToDotNotation(planetModifs);
+        return Planet.findByIdAndUpdate(idPlanet, planetToDotNotation, {new:true});
+    }
+
+    delete(idPlanet) {
+        return Planet.findByIdAndDelete(idPlanet);
+    }
+
 }
 
 export default new PlanetRepository();
--- S04/src/routes/planet.routes.js
+++ S04/src/routes/planet.routes.js
@@ -21,8 +21,20 @@ class PlanetsRoutes {
         router.put('/:idPlanet', this.put);
     }
 
-    patch(red, res, next) {
-        return next(HttpError.NotImplemented());
+    async patch(req, res, next) {
+        try {
+            let planet = await planetRepository.update(req.params.idPlanet, req.body);
+            if(!planet){
+                return next(HttpError.NotFound(`La planète avec l'identifiant ${req.params.idPlanet} n'existe pas`));
+            }
+
+            planet = planet.toObject({getters:false, virtuals:false});
+            planet = planetRepository.transform(planet);
+
+            res.status(200).json(planet);
+        } catch (err) {
+            return next(err);
+        }
     }
 
     put(red, res, next) {
@@ -30,31 +42,38 @@ class PlanetsRoutes {
     }
 
 
-    delete(req, res, next) {
-        const index = PLANETS.findIndex(p => p.id == req.params.idPlanet);
-
-        if (index === -1) {
-            return next(HttpError.NotFound(`La planète avec l'identifiant ${req.params.id} n'existe pas`))
-        } else {
-            PLANETS.splice(index, 1);
+    async delete(req, res, next) {
+        try {
+            const deleteResult = await planetRepository.delete(red.params.idPlanet);
+            if (!deleteResult) {
+                return next(HttpError.NotFound(`La planète avec l'identifiant ${req.params.idPlanet} n'existe pas`));
+            }
             res.status(204).end(); // NO content
+
+        } catch (err) {
+            return next(err);
         }
     }
 
-   async post(req, res, next) {
+    async post(req, res, next) {
         const newPlanet = req.body;
 
+        if(Object.keys(newPlanet).length === 0){
+            return next(HttpError.BadRequest(`La planète ne peut pas contenir aucune donnée`));
+        }
+        
+
         try {
-           let planetAdded = await planetRepository.create(newPlanet);
-            
-           planetAdded = planetAdded.toObject({ getters: false, virtuals: false });
-           planetAdded = planetRepository.transform(planetAdded);
+            let planetAdded = await planetRepository.create(newPlanet);
+
+            planetAdded = planetAdded.toObject({ getters: false, virtuals: false });
+            planetAdded = planetRepository.transform(planetAdded);
 
             res.status(201).json(planetAdded);
         } catch (err) {
             return next(err);
         }
-       
+
     }
 
     async getAll(req, res, next) {
